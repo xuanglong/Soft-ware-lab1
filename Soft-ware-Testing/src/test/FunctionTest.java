91116c6ce5c8b93c2eaae1598788641d5ad309f9
xuanglong <100769809+xuanglong@users.noreply.github.com>
2022-04-06 14:57:25 +0800

    Update FunctionTest.java

--- Soft-ware-Testing/src/test/FunctionTest.java
+++ Soft-ware-Testing/src/test/FunctionTest.java
@@ -18,9 +18,7 @@ class FunctionTest
 	
 	
 	 @DisplayName("Test for function")
-    // 进行参数化测试
     @ParameterizedTest
-    // 参数源（将会一个个输入方法进行测试）
     @ValueSource(doubles = {-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1})
 	public void check_arccos(double x)
 	{
@@ -33,7 +31,6 @@ class FunctionTest
     @ParameterizedTest
     @ValueSource(doubles = {2, -2})
     public void checkIllegalArguments(double x){
-        // 断言，判断是否抛出异常
         assertThrows(IllegalArgumentException.class, ()->{cal.getArcCos(x);});
     }
 }
